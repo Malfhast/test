db7ccb4d6851b55d86e65094a8fcf48a5007c82d
Malfhast <70017830+Malfhast@users.noreply.github.com>
2020-10-04 16:08:00 +0200

    Add files via upload

---
+++ module/blades-helpers.js
@@ -0,0 +1,225 @@
+export class BladesHelpers {
+
+  /**
+   * Removes a duplicate item type from charlist.
+   * 
+   * @param {Object} item_data 
+   * @param {Entity} actor 
+   */
+  static removeDuplicatedItemType(item_data, actor) {
+
+    let distinct_types = ["crew_reputation", "class", "vice", "background", "heritage"];
+    let should_be_distinct = distinct_types.includes(item_data.type);
+    // If the Item has the exact same name - remove it from list.
+    // Remove Duplicate items from the array.
+    actor.items.forEach(i => {
+      let has_double = (item_data.type === i.data.type);
+      if (i.data.name === item_data.name || (should_be_distinct && has_double)) {
+        actor.deleteOwnedItem(i.id);
+      }
+    });
+  }
+
+  /**
+   * _getFormData() override helper.
+   * @param {*} form 
+   */
+  static getFormDataHelper(form, editors) {
+
+    const FD = new FormData(form);
+    const dtypes = {};
+    const editorTargets = Object.keys(editors);
+    
+    // Always include checkboxes
+    for ( let el of form.elements ) {
+      if ( !el.name ) continue;
+
+      // Handle Radio groups
+      if ( form[el.name] instanceof RadioNodeList ) {
+        
+        const inputs = Array.from(form[el.name]);
+        if ( inputs.every(i => i.disabled) ) FD.delete(k);
+        
+        let values = "";
+        let type = "Checkboxes";
+        values = inputs.map(i => i.checked ? i.value : false).filter(i => i);
+        
+        FD.set(el.name, JSON.stringify(values));
+        dtypes[el.name] = 'Radio';
+      }
+
+      // Remove disabled elements
+      else if ( el.disabled ) FD.delete(el.name);
+
+      // Checkboxes
+      else if ( el.type == "checkbox" ) {
+          FD.set(el.name, el.checked || false);
+          dtypes[el.name] = "Boolean";
+      }
+
+      // Include dataset dtype
+      else if ( el.dataset.dtype ) dtypes[el.name] = el.dataset.dtype;
+    }
+
+    // Process editable images
+    for ( let img of form.querySelectorAll('img[data-edit]') ) {
+      if ( img.getAttribute("disabled") ) continue;
+      let basePath = window.location.origin+"/";
+      if ( ROUTE_PREFIX ) basePath += ROUTE_PREFIX+"/";
+      FD.set(img.dataset.edit, img.src.replace(basePath, ""));
+    }
+
+    // Process editable divs (excluding MCE editors)
+    for ( let div of form.querySelectorAll('div[data-edit]') ) {
+      if ( div.getAttribute("disabled") ) continue;
+      else if ( editorTargets.includes(div.dataset.edit) ) continue;
+      FD.set(div.dataset.edit, div.innerHTML.trim());
+    }
+
+    // Handle MCE editors
+    Object.values(editors).forEach(ed => {
+      if ( ed.mce ) {
+        FD.delete(ed.mce.id);
+        if ( ed.changed ) FD.set(ed.target, ed.mce.getContent());
+      }
+    });
+
+    // Record target data types for casting
+    FD._dtypes = dtypes;
+    return FD;
+
+  }
+
+  /**
+   * Add item modification if logic exists.
+   * @param {Object} item_data 
+   * @param {Entity} entity 
+   */
+  static callItemLogic(item_data, entity) {
+
+    if ('logic' in item_data.data) {
+      let logic = JSON.parse(item_data.data.logic);
+
+      if (logic) {
+        // Different logic behav. dep on operator.
+        switch (logic.operator) {
+  
+          // Add when creating.
+          case "addition":
+            entity.update({
+              [logic.attribute]: Number(BladesHelpers.getNestedProperty(entity, "data." + logic.attribute)) + logic.value
+            });
+            break;
+  
+        }
+      }
+
+    }
+
+  }
+
+  /**
+   * Undo Item modifications when item is removed.
+   * @param {Object} item_data 
+   * @param {Entity} entity 
+   */
+  static undoItemLogic(item_data, entity) {
+
+    if ('logic' in item_data.data) {
+      let logic = JSON.parse(item_data.data.logic)
+
+      if (logic) {
+        // Different logic behav. dep on operator.
+        switch (logic.operator) {
+          // Subtract when removing.
+          case "addition":
+            entity.update({
+              [logic.attribute]: Number(BladesHelpers.getNestedProperty(entity, "data." + logic.attribute)) - logic.value
+            });
+            break;
+
+        }
+      }
+    }
+
+  }
+
+  /**
+   * Get a nested dynamic attribute.
+   * @param {Object} obj 
+   * @param {string} property 
+   */
+  static getNestedProperty(obj, property) {
+    return property.split('.').reduce((r, e) => {
+        return r[e];
+    }, obj);
+  }
+
+
+  /**
+   * Add item functionality
+   */
+  static _addOwnedItem(event, actor) {
+
+    event.preventDefault();
+    const a = event.currentTarget;
+    const item_type = a.dataset.itemType;
+
+    let data = {
+      name: randomID(),
+      type: item_type
+    };
+    return actor.createEmbeddedEntity("OwnedItem", data);
+  }
+
+  /**
+   * Get the list of all available ingame items by Type.
+   * 
+   * @param {string} item_type 
+   * @param {Object} game 
+   */
+  static async getAllItemsByType(item_type, game) {
+
+    let list_of_items = [];
+    let game_items = [];
+    let compendium_items = [];
+    
+    game_items = game.items.filter(e => e.type === item_type).map(e => {return e.data});
+
+    let pack = game.packs.find(e => e.metadata.name === item_type);
+    let compendium_content = await pack.getContent();
+    compendium_items = compendium_content.map(e => {return e.data});
+
+    list_of_items = game_items.concat(compendium_items);
+
+    return list_of_items;
+
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Returns the label for attribute.
+   *
+   * @param {string} attribute_name 
+   * @returns {string}
+   */
+  static getAttributeLabel(attribute_name) {
+        // Calculate Dice to throw.
+        let attribute_labels = {};
+        const attributes = game.system.model.Actor.character.attributes;
+        
+        for (var attibute_name in attributes) {
+          attribute_labels[attibute_name] = attributes[attibute_name].label;
+          for (var skill_name in attributes[attibute_name].skills) {
+            attribute_labels[skill_name] = attributes[attibute_name].skills[skill_name].label;
+          }
+    
+        }
+    
+        return attribute_labels[attribute_name];
+  }
+
+  /* -------------------------------------------- */
+
+}
---
+++ module/blades.js
@@ -0,0 +1,178 @@
+/**
+ * A simple and flexible system for world-building using an arbitrary collection of character and item attributes
+ * Author: Atropos
+ * Software License: GNU GPLv3
+ */
+
+// Import Modules
+import { registerSystemSettings } from "./settings.js";
+import { preloadHandlebarsTemplates } from "./blades-templates.js";
+import { bladesRoll, simpleRollPopup } from "./blades-roll.js";
+import { BladesHelpers } from "./blades-helpers.js";
+import { BladesActor } from "./blades-actor.js";
+import { BladesItem } from "./blades-item.js";
+import { BladesItemSheet } from "./blades-item-sheet.js";
+import { BladesActorSheet } from "./blades-actor-sheet.js";
+import { BladesCrewSheet } from "./blades-crew-sheet.js";
+import * as migrations from "./migration.js";
+
+window.BladesHelpers = BladesHelpers;
+
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+Hooks.once("init", async function() {
+  console.log(`Initializing Blades In the Dark System`);
+
+  game.blades = {
+    dice: bladesRoll
+  }
+
+  // Define Roll template.
+  // CONFIG.Dice.template = "systems/blades-in-the-dark/templates/blades-roll.html"
+  // CONFIG.Dice.tooltip = "systems/blades-in-the-dark/templates/blades-roll-tooltip.html"
+
+  CONFIG.Item.entityClass = BladesItem;
+  CONFIG.Actor.entityClass = BladesActor;
+
+  // Register System Settings
+  registerSystemSettings();
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("blades", BladesActorSheet, { types: ["character"], makeDefault: true });
+  Actors.registerSheet("blades", BladesCrewSheet, { types: ["crew"], makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("blades", BladesItemSheet, {makeDefault: true});
+  preloadHandlebarsTemplates();
+
+
+  // Multiboxes.
+  Handlebars.registerHelper('multiboxes', function(selected, options) {
+    
+    let html = options.fn(this);
+
+    if (typeof selected != 'undefined') {
+      selected.forEach(selected_value => {
+        if (selected_value !== false) {
+          const escapedValue = RegExp.escape(Handlebars.escapeExpression(selected_value));
+          const rgx = new RegExp(' value=\"' + escapedValue + '\"');
+          html = html.replace(rgx, "$& checked=\"checked\"");
+        }
+      });
+    }
+    return html;
+  });
+
+  // Trauma Counter
+  Handlebars.registerHelper('traumacounter', function(selected, options) {
+    
+    let html = options.fn(this);
+    var count = selected.length;
+    if (count > 4) count = 4;
+    
+    const rgx = new RegExp(' value=\"' + count + '\"');
+    return html.replace(rgx, "$& checked=\"checked\"");
+
+  });
+
+  // NotEquals handlebar.
+  Handlebars.registerHelper('noteq', (a, b, options) => {
+    return (a !== b) ? options.fn(this) : '';
+  });
+
+  // ReputationTurf handlebar.
+  Handlebars.registerHelper('repturf', (turfs_amount, options) => {
+    let html = options.fn(this);
+    var turfs_amount_int = parseInt(turfs_amount);
+
+    // Can't be more than 6.
+    if (turfs_amount_int > 6) {
+      turfs_amount_int = 6;
+    }
+
+    for (let i = 13 - turfs_amount_int; i <= 12; i++) {
+      const rgx = new RegExp(' value=\"' + i + '\"');
+      html = html.replace(rgx, "$& disabled=\"disabled\"");
+    }
+    return html;
+  });
+
+  Handlebars.registerHelper('crew_vault_coins', (max_coins, options) => {
+
+    let html = options.fn(this);
+    for (let i = 1; i <= max_coins; i++) {
+
+      html += "<input type=\"radio\" id=\"crew-coins-vault-" + i + "\" name=\"data.vault.value\" value=\"" + i + "\"><label for=\"crew-coins-vault-" + i + "\"></label>";
+    }
+
+    return html;
+  });
+
+  Handlebars.registerHelper('crew_experience', (options) => {
+
+    let html = options.fn(this);
+    for (let i = 1; i <= 10; i++) {
+
+      html += '<input type="radio" id="crew-experience-' + i + '" name="data.experience" value="' + i + '" dtype="Radio"><label for="crew-experience-' + i + '"></label>';
+    }
+
+    return html;
+  });
+
+  // Enrich the HTML replace /n with <br>
+  Handlebars.registerHelper('html', (options) => {
+
+    let text = options.hash['text'].replace(/\n/g, "<br />");
+
+    return new Handlebars.SafeString(text);;
+  });
+
+});
+
+/**
+ * Once the entire VTT framework is initialized, check to see if we should perform a data migration
+ */
+Hooks.once("ready", function() {
+
+  // Determine whether a system migration is required
+  const currentVersion = game.settings.get("bitd", "systemMigrationVersion");
+  const NEEDS_MIGRATION_VERSION = 1.0;
+
+  let needMigration = (currentVersion < NEEDS_MIGRATION_VERSION) || (currentVersion === null);
+
+  // Perform the migration
+  if ( needMigration && game.user.isGM ) {
+    migrations.migrateWorld();
+  }
+});
+
+/*
+ * Hooks
+ */
+Hooks.on("preCreateOwnedItem", (parent_entity, child_data, options, userId) => {
+
+  BladesHelpers.removeDuplicatedItemType(child_data, parent_entity);
+
+  return true;
+});
+
+Hooks.on("createOwnedItem", (parent_entity, child_data, options, userId) => {
+
+  BladesHelpers.callItemLogic(child_data, parent_entity);
+  return true;
+});
+
+Hooks.on("deleteOwnedItem", (parent_entity, child_data, options, userId) => {
+  
+  BladesHelpers.undoItemLogic(child_data, parent_entity);
+  return true;
+});
+// getSceneControlButtons
+Hooks.on("renderSceneControls", async (app, html) => {
+  let dice_roller = $('<li class="scene-control" title="Dice Roll"><i class="fas fa-dice"></i></li>');
+  dice_roller.click(function() {
+    simpleRollPopup();
+  });
+  html.append(dice_roller);
+});
